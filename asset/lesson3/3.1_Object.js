32394dc70dbe6ecc396f8d05577795e000d2aa77
lottypro <carlotta.bagna@gmail.com>
2023-03-16 09:28:16 +0100

    Aggiunta lezione 3.2

--- asset/lesson3/3.1_Object.js
+++ asset/lesson3/3.1_Object.js
@@ -1,4 +1,4 @@
-/* OBJECT */
+/* OBJECT PART 1 */
 
 /*
 La necessità di usare gli Object nasce dal fatto che le variabili possono contenete UN SINGOLO VALORE.
@@ -16,15 +16,9 @@ Può essere creato in diversi modi:
 //Possiamo creare un oggetto così:
 
     var animal = {
-        nasce: function () {
-            //...
-        },
-        respira: function () {
-            //...
-        },
-        mangia: function () {
-            //...
-        },
+        nasce: function () {/*...*/},
+        respira: function () {/*...*/},
+        mangia: function () {/*...*/},
     }       // quindi definiamo il nostro "archivio" con delle proprietà definite all'interno parentesi graffe 
 
 // Ma se avessimo la necessità di creare un altro oggetto che erediti queste proprietà lo possiamo fare con Object.create(prototype)
@@ -36,9 +30,8 @@ Può essere creato in diversi modi:
 // a questo punto vediamo che felino ha ereditato le propèrietà di animal e possiamo ulteriormente implementare proprietà:
 
     felino.type = "mammifero";
-    felino.corre = function(){
-        //..
-    }
+    felino.corre = function(){/*...*/}
+    felino.verso = "roar"
 
 // ora oltre ad aver assorbito le proprietà di animal si sono aggiunte type e corre che sono proprie solo di felino.
 
@@ -58,7 +51,6 @@ Può essere creato in diversi modi:
 /* --- USI POSSIBILI DI OBJECT.CREATE & PROTOTYPE --- */
 
 /*
-
 Un possibile uso di Object.create e prototype può essere fatto quando ci si vuole proteggere dalla possibilità che
 altri possano modificare il valore delle properties di un oggetto (specie quando si passa tramite librerie terze
 parti).
@@ -67,76 +59,58 @@ In questi casi usare:
 var o = {x: “should notchange”};
 lib.function(Object.create(o));
 
-In questo modo ogni modifica alla prop x, non impatta (o).
+In questo modo ogni modifica alla propery x, non impatta (o).
+*/
+
+
+/* --- OTTENERE I VALORI DALLE PROPERTIES --- */
+
+/*
+Si accede ai valori delle prop. di un Object attraverso la “dot notation”:
+
+    var book = { author: “pippo” }; var a = book.author;
+
+E’ anche possibile accedere al valore attraverso il property index:
+
+    var a = book[“author”]; che ricorda un array associativo..
+*/
+
+/* --- SETTARE I VALORI NELLE PROPERTIES --- */
+
+/*
+Analogamente è possibile settare i valori delle properties:
+
+    book.author = “pippo1”;
+    book[“author”] = “pippo1”;
+
+Il nome di una property deve essere necessariamente una stringa o un Symbol (può essere anche una stringa vuota!)
+
+E’ possibile assegnare valori a proprietà che ancora non esistono nell’oggetto.
+E’ possibile accedere a proprietà che non esistono nell’oggetto quindi semplicemente il valore di
+ritorno sarà undefined;
+Le properties possono essere readonly (quindi mi permette la sola lettura e non la modifica dell'elemento)
 */
 
 
 
-/* ---  --- */
+/* --- CANCELLARE LE PROPERTIES --- */
 
-/*
-Queste strutture vengono utilizzate per RIPETERE l'esecuzione di uno statement un NUMERO DEFINITO di volte.
-
-I loop possono essere prodotti da:
-
-    - while (true) statement  ->  Lo statement successivo si esegue solo se la condizione è vera (o fino a quando continua ad essere vera);
-    - do ... while (true) statement   ->  Lo statement si esegue subito e finchè la condizione è vera;
-    - for statement   ->  consente la definizione di 3 stati: 
-                              1. la condizione iniziale
-                              2. il controllo (come nel while)
-                              3. il mutamento della condizione
-    - for ... in statement   ->   Consente di ciclare per i membri di un array, oppure di un oggetto.
-    - for ... of statement   ->   Come il precedente, solo che itera su ogni oggetto iterable Array, Map, NodeList e altro.
-
-Poi abbiamo:
-
-- BREAK che può interrompere un loop anche se la condizione di controllo continua ad essere vera;
-- CONTINUE che consente di saltare lo statement successivo e ripetere la condizione di controllo e quindi il loop.
-
-Tra questi indubbiamente il costrutto più utilizzato è il FOR (quindi preferibile usarlo al posto di while o do.while)
-*/
-
-
-
-/* --- TIMERS --- */
-
-/*
-Sono strutture che servono ad eseguire degli statement nel tempo e sono di due tipi:
-
-    - setTimeout(action,delay)  -> che consente di impostare l'esecuzione di uno statement (action) dopo un dato periodo di temo (delay)
-    - setInterval(action,delay) -> che consente di impostare l'esecuzione di uno statement (action) af intervalli programmati (delay)
-*/
-
-
-
-/* --- DATA TYPES & TYPEOF --- */
-
-/*
-I tipi di JS possono essere divisi in 2 macro categorie: 
-
-    - PRIMITIVI 
-        - boolean // typeof -> boolean
-        - number // typeof -> number
-        - string // typeof -> string
-        - null // typeof -> object
-        - undefinded // typeof -> object
-
-    - OBJECT
-      qualsiasi valore JS che non ricada in uno dei tipi primitivi definiti sopra è un Object. Un object in JS è essenzialmente una UNORDERED collection di proprietà, ciascuna delle quali ha un name e un value. (anche le FUNCTION sono object)
-
-Esiste anche uno speciale tipo di Object che consiste in una ordered collection che è l'ARRAY.
-Ci sono anche object speciali come:
+//Con l’operatore “delete” è possibile “cancellare” una property
     
-    - set
-    - map
-    - regExp
-    - date
-    - error
-    - JSON
-    - intl
-    - console
-    - url
+    delete leone.verso
+    console.log(leone)
 
-typeof è un metodo nativo che consente di ispezionare (e ricevere in risposta) il tipo di un valore o
-una variabile. (possiamo usare typeof in console per chiedere il tipo di una variabile o di un valore)
+/* Questa property però verrà eliminata ** SOLO ** non è una proprietà ereditata da un prototipo, 
+infatti leone continua ad avere questa proprietà perchè l'ha ereditata da felino. 
+Può essere usata tranquillamente per eliminare una property di un prototype però bisogna sempre fare ** ATTENZIONE ** a cancellare 
+o modificare le proprietà di un oggetto o ancor di più ad un prototype perchè poi il cambiamento andrà ad impattare tutte le 
+sue eredità. Quindi si consiglia sempre di clonare l'oggetto e modificare il clone.
+Se invece:
 */
+
+    leone.assalto = function(){/*...*/}
+    console.log(leone) //qui vediamo aggiunta la proprietà assalto
+
+    delete leone.assalto
+    console.log(leone) //ora la proprietà viene eliminata
+
---
+++ asset/lesson3/3.2_Object.js
@@ -0,0 +1,9 @@
+/* OBJECT PART 2 */
+
+/* --- TESTARE LE PROPERTIES --- */
+
+//Per verificare se un oggetto possiede o meno una property possiamo usare:
+    // leone.caccia = "..."
+    // leone.hasOwnProperty("caccia")
+    leone.hasOwnProperty("verso")
+    console.log(leone)
